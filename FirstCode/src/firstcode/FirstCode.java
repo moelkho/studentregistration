d43a7a93672d8affdade25406155ed56e9f032cb
elkhomsiMohamed <>
2020-09-07 13:35:02 -0400

    Commit Final Mohamed Elkhomsi

---
+++ FirstCode/src/firstcode/FirstCode.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstcode;
+
+/**
+ *
+ * @author mohamed El Khomsi
+ * anything you want to write in java goes into a Class, the first code we write
+ * is a hello world, we start by that to test our environment is working and 
+ * everything is set up.
+ * in java we have to specify the starting point, and in java we start everytime
+ * with a main method
+ */
+public class FirstCode {
+
+   /*
+    the main is always the starting point, and main() is a method
+    and always has the same syntaxe, 
+    */
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World");
+    }
+    
+}
