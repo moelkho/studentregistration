f654005abd045d4bdb9ece436b008d144a8f8206
admin <admin@DESKTOP-LOSRB3T>
2020-09-07 18:16:47 -0400

    Commit  Mohamed Elkhomsi

---
+++ ArithmeticOperators/src/MainClass.java
@@ -0,0 +1,29 @@
+
+public class MainClass {
+
+  
+    public static void main(String[] args) {
+        
+        /*
+        Arithmatic operators +,-,*,/,%
+        */
+        
+        int num1 = 6;
+        int num2 =5;
+        
+        int addition = num1+num2;
+        int soustraction = num1-num2;
+        int multiplication = num1*num2;
+        
+        double division =(double) num1/num2;
+        
+        int reminder = num1%num2;
+        
+        System.out.println("addition : "+addition);
+        System.out.println("soustratction  : " +soustraction);
+        System.out.println("multiplication :" +multiplication);
+        System.out.println("division : " +division);
+        System.out.println("reminder :" +reminder);
+    }
+    
+}
